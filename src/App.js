0d9a7c55120365464bd9e1e897e8398aa56b3f8f
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-04-03 11:44:13 +0530

    create the folder structure for the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from './components/PageHeader/Index'
+import Navbar from './components/PageNavbar/Index'
+import Body from './components/PageBody/Index'
+import Footer from './components/PageFooter/Index'
 
 function App() {
   return (
     <div className="App">
-      hi
+      <Header></Header>
+      <Navbar></Navbar>
+      <Body></Body>
+      <Footer></Footer>
     </div>
   );
 }
