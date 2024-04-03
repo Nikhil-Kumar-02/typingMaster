d921124cd1e432741ce18abfb528bcc00b07855a
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-04-03 12:12:10 +0530

    created and styled the header of the project clicking fucntionality not added

--- src/components/PageHeader/Index.jsx
+++ src/components/PageHeader/Index.jsx
@@ -1,9 +1,18 @@
 import React from "react"
+import './index.css'
+import logo from '../../images/logo.jpeg'
+import { FaRegKeyboard } from "react-icons/fa";
+import { FaCrown } from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
 
 const Header = (props) => {
   return (
-    <div>
-      header
+    <div className="HeaderContainer">
+      <img src={logo} height={40}></img>
+      <h2>Funky Type</h2>
+      <FaRegKeyboard></FaRegKeyboard>
+      <FaCrown></FaCrown>
+      <IoMdSettings></IoMdSettings>
     </div>
   )
 };
