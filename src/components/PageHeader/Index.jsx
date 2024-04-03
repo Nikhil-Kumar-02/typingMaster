98b799cfad1d881f09ed2c815f7b196a5c1d1eae
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-04-03 13:13:26 +0530

    created and styled the navbar component also added logic to view the current selected option with yellow color

--- src/components/PageHeader/Index.jsx
+++ src/components/PageHeader/Index.jsx
@@ -4,14 +4,16 @@ import logo from '../../images/logo.jpeg'
 import { FaRegKeyboard } from "react-icons/fa";
 import { FaCrown } from "react-icons/fa";
 import { IoMdSettings } from "react-icons/io";
+import { IoInformationOutline } from "react-icons/io5";
 
 const Header = (props) => {
   return (
     <div className="HeaderContainer">
       <img src={logo} height={40}></img>
-      <h2>Funky Type</h2>
+      <h2>FunkyType</h2>
       <FaRegKeyboard></FaRegKeyboard>
       <FaCrown></FaCrown>
+      <IoInformationOutline></IoInformationOutline>
       <IoMdSettings></IoMdSettings>
     </div>
   )
--- src/components/PageNavbar/Index.jsx
+++ src/components/PageNavbar/Index.jsx
@@ -1,9 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
+import './index.css'
+import { PiHashStraightBold } from "react-icons/pi";
+import { MdOutlineAccessTimeFilled } from "react-icons/md";
+import { TbLetterA } from "react-icons/tb";
+import { FaQuoteLeft } from "react-icons/fa6";
+import { HiMiniWrenchScrewdriver } from "react-icons/hi2";
+import { TbMinusVertical } from "react-icons/tb";
 
 const Navbar = (props) => {
+  function invokeRefrence(e){
+    console.log(e.target);
+    e.target.className = "active";
+  }
+
+  const [time , setTime] = useState(15);
+
   return (
-    <div>
-      navbar
+    <div className="navbarContainer">
+      <div>
+          <div onClick={invokeRefrence}>@ punctuation</div>
+          <div onClick={invokeRefrence}><PiHashStraightBold /> numbers</div>
+          <div onClick={invokeRefrence}><MdOutlineAccessTimeFilled/> time</div>
+          <div onClick={invokeRefrence}><TbMinusVertical color="grey"></TbMinusVertical></div>
+          <div onClick={invokeRefrence}><TbLetterA/> words</div>
+          <div onClick={invokeRefrence}><FaQuoteLeft/>quote</div>
+          <div onClick={invokeRefrence}><HiMiniWrenchScrewdriver/>custom</div>
+          <div onClick={invokeRefrence}><TbMinusVertical color="grey"></TbMinusVertical></div>
+          <div onClick={() => setTime(15)} className={time===15 ? "active" : ""}>15</div>
+          <div onClick={() => setTime(30)} className={time===30 ? "active" : ""}>30</div>
+          <div onClick={() => setTime(45)} className={time===45 ? "active" : ""}>45</div>
+          <div onClick={() => setTime(60)} className={time===60 ? "active" : ""}>60</div>
+      </div>
     </div>
   )
 };
