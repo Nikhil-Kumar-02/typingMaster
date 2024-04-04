54b5bdcc8fde2ffe231223c17239ee7c85953f4e
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-04-04 07:24:39 +0530

    added logic that when cursor is somewhere else the para becomes blur and also when we are typing the correctly typed words are colored yellow

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import Header from './components/PageHeader/Index'
 import Navbar from './components/PageNavbar/Index'
 import Body from './components/PageBody/Index'
 import Footer from './components/PageFooter/Index'
+import { useState } from 'react';
 
 function App() {
+  const [focus , setFocus] = useState(true);
   return (
-    <div className="App">
+    <div className="App" onClick={()=>setFocus(false)}>
+
       <Header></Header>
       <Navbar></Navbar>
-      <Body></Body>
+
+      <div onClick={(e)=>{
+        e.stopPropagation()
+        setFocus(true)
+      }}>
+        <Body focus={focus}></Body>
+      </div>
+      
       <Footer></Footer>
     </div>
   );
--- src/components/PageBody/Index.jsx
+++ src/components/PageBody/Index.jsx
@@ -2,20 +2,26 @@ import React, { useEffect, useRef, useState } from "react"
 import './index.css'
 import { TbMinusVertical } from "react-icons/tb";
 
-const Body = (props) => {
+const Body = ({focus}) => {
 
   const inputRef = useRef();
 
-  const text = "With this tool you can delete all punctuation from the given text Punctuation is a set of conventional signs for easing reading and understanding of the written text The most commonly used group of characters are a comma  semicolon  colon  period  exclamation mark  question mark  dash  and brackets There are also other typographic marks such as an ampersand  asterisk  at sign  percent  and many more The tool finds and removes all such marks However if you want to preserve any of the punctuation marks unchanged you can retain them in the text by specifying them in the option for ignored characters For example if you enter the symbol  in the option the sentence structure will remain unchanged If you input several symbols like  then the text will preserve periods commas and semicolons in their original positions Textabulous";
+  let text = "With this tool you can delete all punctuation from the given text Punctuation is a set of conventional signs for easing reading and understanding of the written text The most commonly used group of characters are a comma  semicolon  colon  period  exclamation mark  question mark  dash  and brackets There are also other typographic marks such as an ampersand  asterisk  at sign  percent  and many more The tool finds and removes all such marks However if you want to preserve any of the punctuation marks unchanged you can retain them in the text by specifying them in the option for ignored characters For example if you enter the symbol  in the option the sentence structure will remain unchanged If you input several symbols like  then the text will preserve periods commas and semicolons in their original positions Textabulous";
 
-  let renderedText = text.substring(0,248);
+  const [leftText , setLeftText] = useState(text.substring(0,248))
+  const [typedText , setTypedText] = useState('');
 
   const [index , setIndex] = useState(0);
 
   function textInputHandler(e){
     // console.log(e);
-    if(renderedText[index] === e.key){
+    if(leftText[0] === e.key){
       console.log("char matched")
+      let newTypedText = typedText + leftText[0];
+      setTypedText(newTypedText);
+
+      setLeftText(p => p.substring(1))
+
       setIndex(p => p+1);
     }
     else if(e.key === "Backspace"){
@@ -30,14 +36,22 @@ const Body = (props) => {
 
   useEffect(()=>{
     inputRef.current.focus();
-  },[inputRef.current])
+  },[]);
+
 
   return (
     <div className="bodyWrapper">
-      <div>
-      {/* <TbMinusVertical color="yellow"/> */}
-        <input type="text" onKeyDown={textInputHandler} ref={inputRef}></input>
-        {renderedText}
+      {
+        !focus && <div className="focusMessage">Click Here To focus Again</div>
+      }
+      <div className={focus ? "bodyContentWrapper" : "bodyContentWrapper blurryBackground"} onClick={
+        () => {inputRef.current.focus()}
+      }>
+        <div>
+          <input type="text" onKeyDown={textInputHandler} ref={inputRef}></input>
+          <span style={{color : "yellow", textDecoration : "underline"}}>{typedText}</span>
+          <span>{leftText}</span>
+        </div>
       </div>
     </div>
   )
