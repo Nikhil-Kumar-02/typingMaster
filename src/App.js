52dbb4fda224a66914c4d8cddadcfd0261d7774c
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-09-02 11:18:08 +0530

    found a efficient way to check if the caps lock key is on or not

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
 import './App.css';
-import Header from './components/PageHeader/Index'
-import Navbar from './components/PageNavbar/Index'
-import Body from './components/PageBody/Index'
-import Footer from './components/PageFooter/Index'
-import TestResultDashBoard from './components/TestResult/Index'
-import { useState } from 'react';
+import Header from './components/PageHeader/Index';
+import Navbar from './components/PageNavbar/Index';
+import Body from './components/PageBody/Index';
+import Footer from './components/PageFooter/Index';
+import TestResultDashBoard from './components/TestResult/Index';
+import { useState, useEffect } from 'react';
+import { FaLock } from "react-icons/fa";
 
 function App() {
-  const [time , setTime] = useState(15);
-  const [focus , setFocus] = useState(false);
-  const [testOver , setTestOver] = useState(false);
-  const [typingStats , setTypingStats] = useState({wordsTyped : 0 , incorrectWords : 0});
-  const [charSpeed , setCharSpeed] = useState([]);
+  const [time, setTime] = useState(15);
+  const [focus, setFocus] = useState(false);
+  const [testOver, setTestOver] = useState(false);
+  const [typingStats, setTypingStats] = useState({ wordsTyped: 0, incorrectWords: 0 });
+  const [charSpeed, setCharSpeed] = useState([]);
+  const [capsLockOn, setCapsLockOn] = useState(false); 
 
+  useEffect(() => {
+
+    const handleKeyPress = (event) => {
+      if (event.getModifierState('CapsLock')) {
+        setCapsLockOn(true);
+      } else {
+        setCapsLockOn(false);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+    
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+  
   return (
-    <div className="App" onClick={()=>
-      setFocus(false)
-    }>
+    <div className="App" onClick={() => setFocus(false)}>
+      {/* Conditionally render the Caps Lock warning box */}
+      {capsLockOn && (
+        <div className='capsLockBox'>
+          <FaLock></FaLock> Caps Lock
+        </div>
+      )}
 
       <Header setTestOver={setTestOver}></Header>
-      
-      {
-        testOver ? <TestResultDashBoard charSpeed={charSpeed} typingStats={typingStats} time={time}></TestResultDashBoard> : 
+
+      {testOver ? (
+        <TestResultDashBoard charSpeed={charSpeed} typingStats={typingStats} time={time}></TestResultDashBoard>
+      ) : (
         <>
           <Navbar time={time} setTime={setTime}></Navbar>
 
@@ -29,8 +53,8 @@ function App() {
             <Body setCharSpeed={setCharSpeed} setTypingStats={setTypingStats} setTestOver={setTestOver} focus={focus} time={time} setFocus={setFocus}></Body>
           </div>
         </>
-      }
-      
+      )}
+
       <Footer></Footer>
     </div>
   );
